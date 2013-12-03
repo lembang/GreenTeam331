97d9e1a062ce69159ee0550bbb16620325c2a438
Dylan <dylan@deb.pk.shawcable.net>
2013-12-02 19:26:59 -0800

    divyata tags added

--- SnakesAndLadder/src/GameLogic/Star.java
+++ SnakesAndLadder/src/GameLogic/Star.java
@@ -13,6 +13,13 @@ import SnakesAndLaddersApplet.SnakesAndLadders;
  *
  * @author Divyata
  */
+
+/** CSCI331 DD INTERFACE information of instances can 
+ *  be accessed by my class using setter getter functions.
+ *  constructor and setter handles setting of board number  
+ *  such that the board number of an instance can not be 
+ *  changed outside a range.
+ */
 public class Star {
     private int affectsSquare;
     private int xPos;
@@ -38,8 +45,21 @@ public class Star {
      */
     public int getBoardPosition(){
         return this.affectsSquare;
+    }//CSCI331 INTERFACE getters an
+    public void setBoardPosition(int position){
+       
+        if(position>=1 && position<=100)
+            this.affectsSquare=position;
     }
-    
+
+    /* CSCI331 DD ENCAPSULATION
+     * getters and setters variables is used by my class 
+     * which allows certain variables to be set and can 
+     * and can be used.  My class interfaces in the same 
+     * way and also allows taht we can reuse or retrieve the data 
+     * by calling get functions as well
+     */
+
     Star(){
 	this.affectsSquare = 1 + (int)(Math.random() * (99));
         this.calcDrawXY();
